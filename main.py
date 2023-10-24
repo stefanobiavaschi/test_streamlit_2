252eabba2c4f1f7a7a5698316d846e912c55002b
PorBiavaschiStefano <stefano.biavaschi@porini.it>
2023-10-25 01:31:33 +0200

    test secondo bottone

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ list_path =[r"data/stats-chiav_u15_23_24-vs-annone-22-10-2023.csv",
 def main():
     st.set_page_config(layout="wide")
 
-    st.markdown("# Prova streamlit - stats")
+    st.markdown("# Basket Chiavenna - Statistiche")
     st.markdown(
         """
-        Cartella per provare funzionamento di streamlit
+        Seleziona una squadra basket chiavenna ed una squadra avversaria
         """
     )
     st.markdown("<br>", unsafe_allow_html=True)
@@ -26,44 +26,14 @@ def main():
     list_team = list(set(list(data.my_team.values)))
     list_other = list(set(list(data.other_team.values)))
 
-    scelta = st.radio("Scegli un'opzione:", list_team)
-    data = data.loc[data.my_team == scelta]
-    
+    scelta_team = st.radio("Scegli un'opzione:", list_team)
+    scelta_other = st.radio("Scegli un'opzione:", list_other)
+    data = data.loc[data.my_team == scelta_team]
+    data = data.loc[data.other_team == scelta_other]
+
+    data.drop(columns=["my_team", "other_team", "date"])
     st.write(data)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-import streamlit as st
-
-# Crea un'app Streamlit
-st.title("Seleziona una delle tre opzioni")
-
-# Crea un widget di selezione radio per le tre opzioni
-opzioni = ["Opzione 1", "Opzione 2", "Opzione 3"]
-scelta = st.radio("Scegli un'opzione:", opzioni)
-
-# Variabile per memorizzare la scelta
-variabile_scelta = None
-
-# Gestisci la scelta dell'utente
-if scelta == "Opzione 1":
-    variabile_scelta = "Valore 1"
-elif scelta == "Opzione 2":
-    variabile_scelta = "Valore 2"
-else:
-    variabile_scelta = "Valore 3"
-
-# Mostra il valore scelto
-st.write(f"Valore scelto: {variabile_scelta}")
-
