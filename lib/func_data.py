793fc87a3fb9f7620bf3a6a7305159d1f2278667
PorBiavaschiStefano <stefano.biavaschi@porini.it>
2023-10-25 01:07:39 +0200

    test secondo csv + file func

---
+++ lib/func_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def import_data(list_path):
+    L_append = []
+    for path in list_path:
+        data_temp = pd.read_csv(path).rename(columns={"Nº":"Nr"})
+        data_temp.fillna(9999, inplace=True)
+        data_temp.Nr = data_temp.Nr.astype(int)
+        data_temp.replace(9999, "", inplace=True)
+        game = path.split("/")[-1][6:-4]
+
+        split_ = game.split("-")
+        my_team = split_[0]
+        other_team = split_[2]
+        date = pd.to_datetime( split_[3] + "-" + split_[4] + "-" + split_[5] )
+
+        data_temp["my_team"] = my_team
+        data_temp["other_team"] = other_team
+        data_temp["date"] = date
+
+        L_append = L_append + [data_temp]
+
+    data = pd.concat(L_append)
+    return data
--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-# This demo lets you to explore the Udacity self-driving car image dataset.
-
 import streamlit as st
 import pandas as pd
 import os, urllib
 
-# from lib.io_utils import download_file
-# from lib.ui_components import render_frame_selector_ui, render_object_detector_ui
-# from lib.image_utils import draw_image_with_boxes, load_image
-# from lib.yolo_model import yolo_v3_predict
+from lib.func_data import import_data
 
-
-# from configs import DATA_URL_ROOT, EXTERNAL_DEPENDENCIES
-
-
-# Download a single file and make its content available as a string.
-@st.cache_resource(show_spinner=False)
-def get_file_content_as_string(path):
-    return open(path, "r").read()
+list_path =[r"data/stats-chiav_u15_23_24-vs-annone-22-10-2023.csv",
+             r"data/stats-chiav_dr3_23_34-vs-brembate-22-10-2023.csv"
+            ]
 
 
 def main():
@@ -30,41 +20,14 @@ def main():
     )
     st.markdown("<br>", unsafe_allow_html=True)
 
-    # Download external dependencies.
-    # for filename in EXTERNAL_DEPENDENCIES.keys():
-        # download_file(filename)
 
-    # app_mode = st.radio("Choose the app mode", [ "Show the main.py source code", "Run the app"])
-    # st.markdown("""---""")
-    # if app_mode == "Show the main.py source code":
-    #     st.code(get_file_content_as_string("main.py"))
-    # elif app_mode == "Run the app":
-    #     run()
+    data = import_data(list_path)
 
-    data = pd.read_csv(r"data/stats-chiav_u15_23_24-vs-annone-22-10-2023.csv")
+    list_team = list(set(list(data.my_team.values)))
+    list_other = list(set(list(data.other_team.values)))
+
+    
     st.write(data)
-
-
-
-# This is the main app app itself, which appears when the user selects "Run the app".
-# def run():
-#     @st.cache_data
-#     def create_summary(metadata):
-#         one_hot_encoded = pd.get_dummies(metadata[["frame", "label"]], columns=["label"])
-#         summary = (
-#             one_hot_encoded.groupby(["frame"])
-#             .sum()
-#             .rename(
-#                 columns={
-#                     "label_biker": "biker",
-#                     "label_car": "car",
-#                     "label_pedestrian": "pedestrian",
-#                     "label_trafficLight": "traffic light",
-#                     "label_truck": "truck",
-#                 }
-#             )
-#         )
-#         return summary
 
 
 if __name__ == "__main__":
