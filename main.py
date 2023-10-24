f8908a8a6ceb50e09ee8a80d3787c86a2b5d6013
PorBiavaschiStefano <stefano.biavaschi@porini.it>
2023-10-25 01:25:28 +0200

    test bottone squadra

--- main.py
+++ main.py
@@ -26,9 +26,44 @@ def main():
     list_team = list(set(list(data.my_team.values)))
     list_other = list(set(list(data.other_team.values)))
 
+    scelta = st.radio("Scegli un'opzione:", list_team)
+    data = data.loc[data.my_team == scelta]
     
     st.write(data)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+
+# Crea un'app Streamlit
+st.title("Seleziona una delle tre opzioni")
+
+# Crea un widget di selezione radio per le tre opzioni
+opzioni = ["Opzione 1", "Opzione 2", "Opzione 3"]
+scelta = st.radio("Scegli un'opzione:", opzioni)
+
+# Variabile per memorizzare la scelta
+variabile_scelta = None
+
+# Gestisci la scelta dell'utente
+if scelta == "Opzione 1":
+    variabile_scelta = "Valore 1"
+elif scelta == "Opzione 2":
+    variabile_scelta = "Valore 2"
+else:
+    variabile_scelta = "Valore 3"
+
+# Mostra il valore scelto
+st.write(f"Valore scelto: {variabile_scelta}")
+
