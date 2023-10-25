6467f05c79015927a69f50c852a574e3ca427eb5
PorBiavaschiStefano <stefano.biavaschi@porini.it>
2023-10-25 14:24:38 +0200

    dddd

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ list_path =[
     r"data/stats-chiav_u14_22_23-vs-pontida-05-03-2023.csv",
     r"data/stats-chiav_u15_23_24-vs-annone-22-10-2023.csv",
     r"data/stats-chiav_u17_22_23-vs-mandello-30-04-2023.csv",
-    r"data/stats-chiav_u17_22_23-vs-morbegno_u17-19-02-2023.csv",
+    r"data/stats-chiav_u17_22_23-vs-morbegno-19-02-2023.csv",
     r"data/stats-chiav_u17_22_23-vs-rovagnate-29-01-2023.csv",
     r"data/stats-chiav_1d_22_23-vs-sondrio-05-03-2023.csv"
 ]
