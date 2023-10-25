2bad0b0bf70deb475e99ddaa4676ad35accdc6b3
PorBiavaschiStefano <stefano.biavaschi@porini.it>
2023-10-25 14:26:00 +0200

    1 ala volta

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os, urllib
 from lib.func_data import import_data
 
 list_path =[
-    # r"data/stats-chiav_dr3_23_24-vs-brembate-22-10-2023.csv",
+    r"data/stats-chiav_dr3_23_24-vs-brembate-22-10-2023.csv",
     r"data/stats-chiav_u14_22_23-vs-pontida-05-03-2023.csv",
     r"data/stats-chiav_u15_23_24-vs-annone-22-10-2023.csv",
     # r"data/stats-chiav_u17_22_23-vs-mandello-30-04-2023.csv",
