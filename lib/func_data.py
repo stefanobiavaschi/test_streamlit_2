51392195b65f85fb917a69b5fa8cffcb6542d56c
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2023-10-29 15:14:54 +0100

    tolto st da fanc_data

--- lib/func_data.py
+++ lib/func_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import base64
-import streamlit as st
 import os
 
 def import_data():
@@ -55,14 +54,3 @@ def sec_to_time(sec):
     min = int( sec // 60 )
     sec_new = int( sec - (min*60) )
     return f"{min}:{sec_new}"
-
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # Embedding PDF in HTML
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
