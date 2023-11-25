9d1e444529649f4ba948c842327cfe5b9dc35ac3
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2023-11-25 19:32:50 +0100

    'FTA', 'OREB', 'DREB', 'REB',

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ def main():
         list_player = list(set(list(data.loc[(data.my_team == scelta_team) & (data.season == scelta_season)].Giocatore.values)))
         st.markdown("### Storico per voce statistica:")
         scelta_player = st.radio("Giocatore:", list_player, horizontal=True)
-        list_feat = ['MIN', 'PTS', 'FGM', 'FGA',  '3PM', '3PA', '2PM', '2PA','FTM', 
-                     'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'SR', 'PF', 'PIR', 'EFF' ] # 'FG%','3P%', '2P%', 
+        list_feat = ['MIN', 'PTS', 'FGM', 'FGA',  '3PM', '3PA', '2PM', '2PA','FTM', 'FTA', 'OREB', 'DREB', 'REB',
+                    'AST', 'TOV', 'STL', 'BLK', 'SR', 'PF', 'PIR', 'EFF' ] # 'FG%','3P%', '2P%', , 'FT%'
         scelta_feat = st.radio("Voci statistiche:", list_feat, horizontal=True)
         st.markdown(f"Storico per {scelta_feat} - {scelta_player}:")
         data_plot = data.loc[(data.season == scelta_season) & (data.my_team == scelta_team) &  (data.Giocatore == scelta_player)]
