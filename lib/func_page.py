1a11b33d486fb1b0198038ba52c22021ae1876b4
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2024-01-13 16:47:22 +0100

    squadra selez

--- lib/func_page.py
+++ lib/func_page.py
@@ -57,8 +57,11 @@ def singola():
 
     data = data.loc[ (data.my_team == st.session_state.scelta_team) & (data.season == st.session_state.scelta_season ) ]
 
+    col01, col02 = st.columns(2)
+    col01.button("🔙Home", on_click=set_home)
+    col02.write(f"### Squadra selezionata: {st.session_state.scelta_team} - {st.session_state.scelta_season} ")
+
     col11, col12 = st.columns(2)
-    col12.button("🔙Home", on_click=set_home)
 
     list_other = list(set(list(data.loc[(data.my_team == st.session_state.scelta_team) & (data.season == st.session_state.scelta_season)].other_team.values)))
     scelta_other = col11.radio("Nemico:", list_other, horizontal=True)
