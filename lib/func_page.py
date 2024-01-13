697c8c7b19d6fd0f0e0af60b0f5573f1f1104c8e
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2024-01-13 16:40:24 +0100

    visualizza

--- lib/func_page.py
+++ lib/func_page.py
@@ -32,6 +32,7 @@ def home():
     data = data.loc[ (data.my_team == st.session_state.scelta_team) & (data.season == st.session_state.scelta_season ) ]
 
     col11, col12 = st.columns(2)
+    col12.markdown("### Visualizza:")
     col12.button("Statistiche partite", on_click=set_singola)
     col12.button("Statistiche aggregate")
 
