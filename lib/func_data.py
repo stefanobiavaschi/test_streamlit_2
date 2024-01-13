a117350ec730ec67936e3b2078db7daeb377f2ab
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2024-01-13 18:09:35 +0100

    <span class="navbar-brand">Basket Chiavenna</span>

--- lib/func_data.py
+++ lib/func_data.py
@@ -101,6 +101,7 @@ def header():
     st.markdown(f"""<nav class="navbar fixed-top navbar-expand-lg " style="background-color: #FFFFFF;">
     <div class="" style="margin-right: 15px;">
     </div>
+    <span class="navbar-brand">Basket Chiavenna</span>
     <img  class="float-right" src="data:image/png;base64,{img_to_bytes(img_Blue)}" class="img-fluid" style="max-height: 60px;">
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
