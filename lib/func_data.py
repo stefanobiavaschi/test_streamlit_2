5ac87b548445c41f5e38b11a49f3031108065af7
stefanobiavaschi <stefano.biavaschi9@gmail.com>
2024-01-13 18:13:08 +0100

    scritta in centro

--- lib/func_data.py
+++ lib/func_data.py
@@ -102,7 +102,7 @@ def header():
     <div class="" style="margin-right: 15px;">
     </div>
     <img  class="float-right" src="data:image/png;base64,{img_to_bytes(img_Blue)}" class="img-fluid" style="max-height: 60px;">
-    <span class="navbar-brand" style="color: orange;">Basket Chiavenna</span>
+    <span class="navbar-brand" style="color: FF8800; text-align: center;">Basket Chiavenna</span>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
